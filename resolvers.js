50a6ad296f07f713fad331a89108e9982976f7af
SergioA01 <sergioleonardoaguirrevargas@gmail.com>
2024-03-12 19:24:16 -0500

    commit dos Sergio Aguirre

--- resolvers.js
+++ resolvers.js
@@ -4,15 +4,12 @@ const resolvers = {
     Query: {
       obtenerPlatos: async (_, { id }) => {
         try {
-          const response = await axios.get(`https://api.example.com/data/${id}`);
+          const response = await axios.get(`https://api-dishes.vercel.app/${id}`);
           
-          // Supongamos que la respuesta de la API es un objeto que tiene una propiedad 'plato'
           const platoData = response.data.plato; 
   
-          // Asegúrate de que 'platoData' coincida con la estructura de tipo 'Platos'
           const plato = {
             id: platoData.id,
-            // Otros campos de 'Platos' según la respuesta de la API
           };
   
           return plato;
